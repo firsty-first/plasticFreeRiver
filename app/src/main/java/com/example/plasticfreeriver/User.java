82fb5803b352548a1ac1606bc2ac97b403730bfa
Gaurav Singh <gks.21u10252@btech.nitdgp.ac.in>
2023-08-02 03:09:58 +0530

    added users class

--- app/src/main/java/com/example/plasticfreeriver/User.java
+++ app/src/main/java/com/example/plasticfreeriver/User.java
@@ -1,4 +1,36 @@
 package com.example.plasticfreeriver;
 
 public class User {
+    String userId,name,profile;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public User(String userId, String name, String profile) {
+        this.userId = userId;
+        this.name = name;
+        this.profile = profile;
+
+    }
 }
