4ef0fcbcb85a49ef8f7fe07d47309165b8c36b21
Gaurav Singh <gks.21u10252@btech.nitdgp.ac.in>
2023-08-26 22:22:26 +0530

    fetch

--- app/src/main/java/com/example/plasticfreeriver/MyAdapter.java
+++ app/src/main/java/com/example/plasticfreeriver/MyAdapter.java
@@ -1,4 +1,54 @@
 package com.example.plasticfreeriver;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.plasticfreeriver.databinding.RvFeedcardBinding;
+
+import java.util.ArrayList;
+
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.viewHolder>
+{
+Context context;
+ArrayList<post> list;
+
+    public MyAdapter(Context context, ArrayList<post> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyAdapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view= LayoutInflater.from(context).inflate(R.layout.rv_feedcard,parent,false);
+       return new viewHolder(view);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.viewHolder holder, int position) {
+post model=list.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+    public class viewHolder extends RecyclerView.ViewHolder
+    {
+RvFeedcardBinding binding;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding=RvFeedcardBinding.bind(itemView);
+
+
+        }
+    }
 }
--- app/src/main/java/com/example/plasticfreeriver/feedFragment.java
+++ app/src/main/java/com/example/plasticfreeriver/feedFragment.java
@@ -2,11 +2,21 @@ package com.example.plasticfreeriver;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +33,10 @@ public class feedFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    ArrayList<post> ar;
+    RecyclerView recyclerView;
+    FirebaseDatabase database;
+
 
     public feedFragment() {
         // Required empty public constructor
@@ -45,7 +59,6 @@ public class feedFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,7 +71,29 @@ public class feedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View view=inflater.inflate(R.layout.fragment_feed, container, false);
+        database=FirebaseDatabase.getInstance();
+        ar=new ArrayList<>();
+        recyclerView=view.findViewById(R.id.rv_feed);
+        MyAdapter postadapter=new MyAdapter(getContext(), ar);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        database.getReference().child("posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot:snapshot.getChildren())
+                {
+                  post p=  dataSnapshot.getValue(post.class);
+                    ar.add(p);
+                }
+                postadapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return view;
     }
 }
